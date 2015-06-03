5df22e577b673a4fe9fdb44a15eff27334de64c8
Daniel <djleibovic@gmail.com>
2015-06-03 01:02:03 -0400

    Update layout tests for logged in and logged out users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,32 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @non_admin = users(:archer)
+  end
+
+  test "layout links when not logged in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links when logged in" do
+    log_in_as(@non_admin)
+    assert is_logged_in?
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", login_path, count: 0
   end 
+
 end
