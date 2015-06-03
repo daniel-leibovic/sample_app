b80de9034e42e7b10f8d669a0ee5f37405eac349
Daniel <djleibovic@gmail.com>
2015-06-03 00:53:14 -0400

    Test that forwarding url deletes after successful friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -37,6 +37,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name: name,
@@ -48,5 +49,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+
   end
 end
